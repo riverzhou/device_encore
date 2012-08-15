8f507e342d5a4a7057dc90132bc9c3df7d6a258b
river <riverzhou2000@gmail.com>
2012-08-15 11:30:41 +0800

    fix name

--- cm.mk
+++ cm.mk
@@ -7,13 +7,13 @@
 # Inherit some common cyanogenmod stuff.
 $(call inherit-product-if-exists, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit device configuration for Kindle Fire
+# Inherit device configuration for Nook Color
 $(call inherit-product, device/bn/encore/full_encore.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/bn/encore/overlay/cm
 
 PRODUCT_NAME := cm_encore
-PRODUCT_DEVICE := otter
+PRODUCT_DEVICE := encore
 PRODUCT_BRAND := BN
 PRODUCT_MODEL := Nook Color
 PRODUCT_MANUFACTURER := BN
