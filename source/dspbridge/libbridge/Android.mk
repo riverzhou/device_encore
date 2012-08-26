882f9fff3facb42f19c9619d57145b95b8409b65
river <riverzhou2000@gmail.com>
2012-08-26 12:08:06 +0800

    clean files

--- source/dspbridge/libbridge/Android.mk
+++ source/dspbridge/libbridge/Android.mk
@@ -15,7 +15,7 @@ LOCAL_SRC_FILES:= \
 	dsptrap.c
 
 LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/inc	
+	$(LOCAL_PATH)/../inc
 
 LOCAL_CFLAGS += -pipe -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DOMAP_3430 -fpic
 
