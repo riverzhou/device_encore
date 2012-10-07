20192e4eda5acf24d73f920b5a031a7050954845
River Zhou <riverzhou2000@gmail.com>
2012-10-07 17:22:39 +0800

    adjust dalvik heap setting

--- device.mk
+++ device.mk
@@ -237,9 +237,6 @@ PRODUCT_COPY_FILES += \
 ##############################################
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    dalvik.vm.heapstartsize=5m \
-    dalvik.vm.heapgrowthlimit=42m \
-    dalvik.vm.heapsize=128m \
     drm.service.enabled=true \
     persist.service.adb.enable=1 \
     persist.sys.purgeable_assets=1 \
@@ -264,3 +261,4 @@ PRODUCT_CHARACTERISTICS 	:= tablet
 DEVICE_PACKAGE_OVERLAYS 	:= device/bn/encore/overlay
 PRODUCT_TAGS 			+= dalvik.gc.type-precise
 
+$(call inherit-product-if-exists, frameworks/native/build/tablet-dalvik-heap.mk)
